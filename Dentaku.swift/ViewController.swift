65827772740069123fadf44badc68b6856d199ef
平岡 建 <hiraokatatsuru@macbook-pro.local>
2018-05-08 06:52:39 +0900

    バグ消せず

--- Dentaku.swift/ViewController.swift
+++ Dentaku.swift/ViewController.swift
@@ -10,30 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var formulaLabel :UILabel!
-    var operationBtnStr:String = ""
     var isOperation :Bool = false
     var isInput :Bool = false
-    var count :Int = 0
-    var firstInputStrNum: Double = 0
-    var secondInputStrNum: Double = 0
-    var thirdInputStrNum: Double = 0
+    var isEqual:Bool = false
+    var inputNums:[Double] = []
+    var operations:[String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         formulaLabel.text = "0"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
     
     @IBAction func acButtonPush(sender:UIButton){
-        count = 0
         formulaLabel.text = "0"
-        operationBtnStr = ""
         isOperation = false
         isInput = false
+        isEqual = false
+        inputNums.removeAll()
+        operations.removeAll()
     }
     
     //数字入力
@@ -53,6 +52,7 @@ class ViewController: UIViewController {
             if isOperation{//演算子を選んだ直後
                 formulaLabel.text = ""
                 isOperation = false
+                
             }
             formulaLabel.text = formulaLabel.text! + sender.titleLabel!.text!
             print("input3")
@@ -80,300 +80,137 @@ class ViewController: UIViewController {
     
     //+,-,×,÷,%,ボタン
     @IBAction func operationButtonSelect(sender:UIButton){
-        if isInput {//数字が入力された直後
+        var result:Double = NSString(string: formulaLabel.text!).doubleValue
+        if isEqual || isInput == false {
+            //operationsの最後の要素をsender.titleLabel?.text!に入れ替える
+            operations[operations.count-1] = (sender.titleLabel?.text!)!
+            //inputNums.remove(at: 1)
+            isEqual = false
+            isOperation = true
+            print("演算子訂正")
+        }else if isInput {//数字が入力された直後
             isInput = false
             isOperation = true
-            switch count {
-            case 0://１回目の選択(1+
-                //選択した演算子のボタンのタイトルラベル文字列が入る
-                operationBtnStr = (sender.titleLabel?.text)!
-                //現在、表示されている数字をDoubleにして１番目として保存
-                firstInputStrNum = NSString(string: formulaLabel.text!).doubleValue
-                print(firstInputStrNum)
-            case 1://２回目の選択(1+2×
-                //最初の演算子の文字列に選択した演算子の文字列をたす
-                operationBtnStr = operationBtnStr + (sender.titleLabel?.text!)!
-                //現在、表示されている数字をDoubleにして２番目として保存
-                secondInputStrNum = NSString(string: formulaLabel.text!).doubleValue
-                print(secondInputStrNum)
-            case 2://３回目(1+2×3+
-                operationBtnStr = operationBtnStr + (sender.titleLabel?.text!)!
-                //現在、表示されている数字をDoubleにして３番目として保存
-                thirdInputStrNum = NSString(string: formulaLabel.text!).doubleValue
-                print(thirdInputStrNum)
-            default:
-                break
-            }
-            count += 1
-        }else {//operationButtonSelectの連打 演算子が訂正（再選択）された場合
-            switch count{
-            case 1:
-                operationBtnStr = (sender.titleLabel?.text)!
-            case 2://operationBtnStrの末尾１文字を削除し、新しく選択された演算子を追加する
-                let text = operationBtnStr.prefix((operationBtnStr.count)-1)
-                operationBtnStr = text + (sender.titleLabel?.text!)!
-                print(operationBtnStr)
-            case 3:
-                let text = operationBtnStr.prefix((operationBtnStr.count)-1)
-                operationBtnStr = text + (sender.titleLabel?.text!)!
-                print(operationBtnStr)
-            default://数字が入力されてない状態で演算子のみ選択されたら
-                //選択した演算子のボタンのタイトルラベル文字列が入る
-                operationBtnStr = (sender.titleLabel?.text)!
-                firstInputStrNum = 0
-                count = 1
-            }
-            //countは+1されない
+            //選択した演算子のボタンのタイトルラベル文字列が入る
+            operations.append((sender.titleLabel?.text)!)
+            //現在、表示されている数字をDoubleにして１番目として保存
+            inputNums.append(NSString(string: formulaLabel.text!).doubleValue)
+            print("数字が入力された直後")
         }
+//        else {//operationButtonSelectの連打 演算子が訂正（再選択）された場合
+//            if inputNums.count != 0 && operations.count != 0 {
+//                //operationsの最後の要素をsender.titleLabel?.text!に入れ替える
+//                operations[operations.count-1] = (sender.titleLabel?.text!)!
+//                print("演算子訂正")
+//            }
+//        }
+//        let zipped = zip(inputNums, operations + [""])
+//        let equation = zipped.reduce(""){ $0 + $1.0 + $1.1 }
+//        print(zipped)
+        //print(equation)
         
-        print(count)
-        
-        var result:Double = NSString(string: formulaLabel.text!).doubleValue
-        
-        switch operationBtnStr {
-        case "++"://1+2+
-            result = firstInputStrNum + secondInputStrNum
-            print(firstInputStrNum,secondInputStrNum)
-            firstInputStrNum = result
-            operationBtnStr = "+"
-            count = 1
-            print(result)
-        case "+-"://1+2-
-            result = firstInputStrNum + secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "-"
-            count = 1
-        case "-+"://1-2+
-            result = firstInputStrNum - secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "+"
-            count = 1
-        case "--"://1-2-
-            result = firstInputStrNum - secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "-"
-            count = 1
-        case "+×","+÷","-×","-÷"://1+2× or 1+2÷ or 1-2× or 1-2÷
-            return
-        case "×+"://1×2+
-            result = firstInputStrNum * secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "+"
-            count = 1
-        case "×-"://1×2-
-            result = firstInputStrNum * secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "-"
-            count = 1
-        case "××"://1×2×
-            result = firstInputStrNum * secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "*"
-            count = 1
-        case "×÷"://1×2÷
-            result = firstInputStrNum * secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "÷"
-            count = 1
-        case "÷+"://1÷2+
-            result = firstInputStrNum / secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "+"
-            count = 1
-        case "÷-"://1÷2-
-            result = firstInputStrNum / secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "-"
-            count = 1
-        case "÷×"://1÷2×
-            result = firstInputStrNum / secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "×"
-            count = 1
-        case "÷÷"://1÷2÷
-            result = firstInputStrNum / secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "÷"
-            count = 1
-        case "+×+"://1+2×3+
-            result = secondInputStrNum * thirdInputStrNum
-            result = firstInputStrNum + result
-            firstInputStrNum = result
-            operationBtnStr = "+"
-            count = 1
-        case "+×-"://1+2×3-
-            result = secondInputStrNum * thirdInputStrNum
-            result = firstInputStrNum + result
-            firstInputStrNum = result
-            operationBtnStr = "-"
-            count = 1
-        case "+××"://1+2×3×
-            result = secondInputStrNum * thirdInputStrNum
-            secondInputStrNum = result//1+6×
-            operationBtnStr = "+×"
-            count = 2
-        case "+×÷"://1+2×3÷
-            result = secondInputStrNum * thirdInputStrNum
-            secondInputStrNum = result//1+6÷
-            operationBtnStr = "+÷"
-            count = 2
-        case "+÷+"://1+2÷3+
-            result = secondInputStrNum / thirdInputStrNum
-            result = firstInputStrNum + result
-            firstInputStrNum = result
-            operationBtnStr = "+"
-            count = 1
-        case "+÷-"://1+2÷3-
-            result = secondInputStrNum / thirdInputStrNum
-            result = firstInputStrNum + result
-            firstInputStrNum = result
-            operationBtnStr = "-"
-            count = 1
-        case "+÷×"://1+2÷3×
-            result = secondInputStrNum / thirdInputStrNum
-            secondInputStrNum = result
-            operationBtnStr = "+×"
-            count = 2
-        case "+÷÷"://1+2÷3÷
-            result = secondInputStrNum / thirdInputStrNum
-            secondInputStrNum = result
-            operationBtnStr = "+÷"
-            count = 2
-        case "-×+"://1-2×3+
-            result = secondInputStrNum * thirdInputStrNum
-            result = firstInputStrNum - result
-            firstInputStrNum = result
-            operationBtnStr = "+"
-            count = 1
-        case "-×-"://1-2×3-
-            result = secondInputStrNum * thirdInputStrNum
-            result = firstInputStrNum - result
-            firstInputStrNum = result
-            operationBtnStr = "-"
-            count = 1
-        case "-××"://1-2×3×
-            result = secondInputStrNum * thirdInputStrNum
-            secondInputStrNum = result//1-6×
-            operationBtnStr = "-×"
-            count = 2
-        case "-×÷"://1-2×3÷
-            result = secondInputStrNum * thirdInputStrNum
-            secondInputStrNum = result//1-6÷
-            operationBtnStr = "-÷"
-            count = 2
-        case "-÷+"://1-2÷3+
-            result = secondInputStrNum / thirdInputStrNum
-            result = firstInputStrNum - result
-            firstInputStrNum = result
-            operationBtnStr = "+"
-            count = 1
-        case "-÷-"://1+2÷3-
-            result = secondInputStrNum / thirdInputStrNum
-            result = firstInputStrNum - result
-            firstInputStrNum = result
-            operationBtnStr = "-"
-            count = 1
-        case "-÷×"://1-2÷3×
-            result = secondInputStrNum / thirdInputStrNum
-            secondInputStrNum = result//1-0.66666667×
-            operationBtnStr = "-×"
-            count = 2
-        case "-÷÷"://1-2÷3÷
-            result = secondInputStrNum / thirdInputStrNum
-            secondInputStrNum = result//1-0.6666667÷
-            operationBtnStr = "-÷"
-            count = 2
+//        print(operations)
+//        print(operations.count)
+        result = caluculate(operation: self.operations, inputNum: self.inputNums)
+        let str: String = String(format: "%g", result)
+        formulaLabel.text = str
+    }
+    
+    func caluculate(operation: [String], inputNum: [Double]) -> Double {
+        var returnVal: Double = NSString(string: formulaLabel.text!).doubleValue
+        if operation.count == 2 {
+            guard (operations[0] == "+" || operations[0] == "-") && (operations[1] == "×" || operations[1] == "÷") else {
+                //計算してreturnValに格納
+                returnVal = calc4Ways(num1: inputNum[0], num2: inputNum[1], opr: operation[0])
+                print("guard")
+                return returnVal
+            }
+            print("operation.count == 2")
+        }else if operation.count == 3 {
+            returnVal = calc4Ways2(num1: inputNum[0], num2: inputNum[1],num3: inputNum[2], opr1: operation[0], opr2: operation[1])
+            print("operation.count == 3")
+        }
+        return returnVal
+    }
+    
+    func calc4Ways(num1: Double, num2: Double, opr: String) -> Double{
+        var returnVal: Double = 0
+        switch opr {
+        case "+":
+            returnVal = num1 + num2
+        case "-":
+            returnVal = num1 - num2
+        case "×":
+            returnVal = num1 * num2
+        case "÷":
+            returnVal = num1 / num2
         default:
             break
         }
-        let str: String = String(format: "%.\(String(describing: result))f", result)
-        formulaLabel.text = str
+        //計算結果を配列の適切な要素番号に格納
+        inputNums[0] = returnVal
+        //演算子が最後の文字の場合1+2-
+        if isOperation {
+            operations[0] = operations[1]
+            //いらなくなった配列の要素を削除(removeAtIndex(n))
+            inputNums.remove(at: 1)//1+2=の場合は+2を残すので削除しない
+        }
+        operations.remove(at: 1)
+        print(operations)
+        print(operations.count)
+        return returnVal
+    }
+    
+    func calc4Ways2(num1: Double, num2: Double,num3: Double, opr1: String, opr2: String) -> Double{
+        var returnVal: Double = 0
+        let str = opr1 + opr2
+        switch str {
+        case "+×":
+            returnVal = num1 + num2 * num3
+        case "-×":
+            returnVal = num1 - num2 * num3
+        case "+÷":
+            returnVal = num1 + num2 / num3
+        case "-÷":
+            returnVal = num1 - num2 / num3
+        default:
+            break
+        }
+        //計算結果を配列の適切な要素番号に格納
+        inputNums[0] = returnVal
+        //演算子が最後の文字の場合1+2×3+
+        if isOperation {
+            operations[0] = operations[2]
+            //いらなくなった配列の要素を削除(removeAtIndex(n))
+            inputNums.removeSubrange(1...2)
+        }
+        else {//1+2×3=
+            inputNums[1] = inputNums[2]
+            operations[0] = operations[1]
+            inputNums.remove(at: 2)
+        }
+        operations.removeSubrange(1...2)
+        print(operations)
+        print(operations.count)
+        return returnVal
     }
     
     //=ボタン
     @IBAction func equalButtonAnswer(sender:UIButton){
-        switch count {
-        case 1://1+2= or 1+=
-            //現在、表示されている数字をDoubleにして２番目として保存
-            secondInputStrNum = NSString(string: formulaLabel.text!).doubleValue
-            print(secondInputStrNum)
-        case 2://1+2×3= or 1+2×3+=
-            //現在、表示されている数字をDoubleにして３番目として保存
-            thirdInputStrNum = NSString(string: formulaLabel.text!).doubleValue
-            print(thirdInputStrNum)
-        default:
-            break
-        }
-        
         var result:Double = NSString(string: formulaLabel.text!).doubleValue
-        
-        switch operationBtnStr {
-        case "+"://1+2= or 1+=
-            result = firstInputStrNum + secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "+"
-        case "-"://1-=
-            result = firstInputStrNum - secondInputStrNum
-            firstInputStrNum = result
-            operationBtnStr = "-"
-        case "×"://1×=
-            result = firstInputStrNum * secondInputStrNum
-            firstInputStrNum = result
-        case "÷"://1÷=
-            result = firstInputStrNum / secondInputStrNum
-            firstInputStrNum = result
-        case "+×"://1+2*3=
-            result = secondInputStrNum * thirdInputStrNum
-            secondInputStrNum = result
-            result = firstInputStrNum + secondInputStrNum
-            firstInputStrNum = result
-            secondInputStrNum = thirdInputStrNum
-            operationBtnStr = "×"//7*3
-        case "+÷"://1+2÷3=
-            result = secondInputStrNum / thirdInputStrNum
-            secondInputStrNum = result
-            result = firstInputStrNum + secondInputStrNum
-            firstInputStrNum = result
-            secondInputStrNum = thirdInputStrNum
-            operationBtnStr = "÷"//1.66666667÷3
-        case "-×"://1-2×3=
-            result = secondInputStrNum * thirdInputStrNum
-            secondInputStrNum = result
-            result = firstInputStrNum - secondInputStrNum
-            firstInputStrNum = result
-            secondInputStrNum = thirdInputStrNum
-            operationBtnStr = "÷"//-5×3
-        case "-÷"://1-2÷3=
-            result = secondInputStrNum / thirdInputStrNum
-            secondInputStrNum = result
-            result = firstInputStrNum - secondInputStrNum
-            firstInputStrNum = result
-            secondInputStrNum = thirdInputStrNum
-            operationBtnStr = "÷"//0.33333333÷3
-        default:
-            break
+        if inputNums.count >= 1{
+            isEqual = true
+            //選択した演算子のボタンのタイトルラベル文字列が入る
+            operations.append((sender.titleLabel?.text)!)
+            inputNums.append(NSString(string: formulaLabel.text!).doubleValue)
+            print(operations)
+            print(inputNums)
+            //計算してreturnValに格納
+            result = caluculate(operation: self.operations, inputNum: self.inputNums)
         }
-        count = 0
-        
-//        //let number = 50
-//        let formatter = NumberFormatter()
-//        formatter.numberStyle = .spellOut
-//        let str = formatter.string(from: result as NSNumber) ?? ""
-        
-//        let hoge :NSNumber = result as NSNumber
-//        //hoge = NSNumber(result)
-//        let str: String = String(format:"%f", hoge.doubleValue)
-//        formulaLabel.text = str
-        
-        //let dot: String = String(result).components(separatedBy: ".")
-        
-        let str: String = String(format: "%.\(String(describing: result))f", result)
-//        if str.hasPrefix("."){
-//            str = String(format: "%.1f", result)
-//        }else {
-//            str = String(format: "%d", result)
-//        }
+        let str: String = String(format: "%g", result)
         formulaLabel.text = str
     }
 }
+
 
